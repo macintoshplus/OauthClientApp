b8875f15135450a18e8491617eddd38a040f323b
macintoshplus <jean-baptiste@nahan.fr>
2014-12-18 16:18:14 +0100

    use only hwi/HWIOAuthBundle for auth user and provide user

--- src/Jbnahan/MyAppBundle/Controller/DefaultController.php
+++ src/Jbnahan/MyAppBundle/Controller/DefaultController.php
@@ -10,4 +10,8 @@ class DefaultController extends Controller
     {
         return $this->render('JbnahanMyAppBundle:Default:index.html.twig', array('name' => $name));
     }
+    public function indexWorldAction()
+    {
+        return $this->render('JbnahanMyAppBundle:Default:index.html.twig', array('name' => 'World'));
+    }
 }
